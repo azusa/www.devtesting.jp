5fe9d9cd08cafe40b33ec16e7c713bdbf0bf39f3
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2024-10-03 05:42:55 +0000

    Initial Commit

---
+++ src/main/java/jp/devtesting/web/Function.java
@@ -0,0 +1,50 @@
+package jp.devtesting.web;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        final String c = request.getQueryParameters().get("c");
+        String target = "";
+        if (c != null) {
+            if (c.equals("index")){
+                target = "page_list.html";
+            } else {
+                target = c + ".html";
+            }
+        }
+        // クエリーパラメーター全体を取得
+        final String queryString = request.getUri().getQuery();
+
+        if (queryString != null) {
+            target = queryString + ".html";
+        }
+        return request.createResponseBuilder(HttpStatus.PERMANENT_REDIRECT).header("Location", "https://archive-devtesting-jp.github.io/tddbc/" + target).build();
+    }
+}
